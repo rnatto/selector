33332c830dfba908dd4183fd53af54c8a9933609
renato <renatobarbosacandido@gmail.com>
2019-11-29 07:55:12 -0200

    Adiciona rfecv

--- selector.py
+++ selector.py
@@ -33,13 +33,13 @@ stat = [i[0] for i in result['attributes'] if i[1] == 'STRING']
 df = pd.DataFrame.from_dict(result['data'])
 
 df.columns = col
-print(df.shape)
+# print(df.shape)
 # drop not used features
 drop_features = [
     'URL',
     'id',
     'tagName',
-    # 'childsNumber',
+    'childsNumber',
     # 'textLength',
     'basePlatform',
     'targetPlatform',
@@ -89,15 +89,20 @@ Y = df.Result
 min_max_scaler = preprocessing.MinMaxScaler()
 # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 # print(min_max_scaler.fit_transform(X))
-X = min_max_scaler.fit_transform(X)
+# X = min_max_scaler.fit_transform(X)
 
-# X = pd.get_dummies(X)
+X = pd.get_dummies(X)
 k_values = [
-    5, 10, 15, 20, 25, 30, 'all'
+    10, 20
+    300, 400,
+    500,
+    1000, 2000, 3000, 4000, 5000
+    # 10000, 15000, 20000, 'all'
 ]
 methods = [f_classif, chi2]
 classifiers = [
-    LogisticRegression(random_state=0, solver='saga', multi_class='multinomial'),
+    LogisticRegression(random_state=0, solver='saga',
+                       multi_class='multinomial'),
     DecisionTreeClassifier(random_state=0),
     LinearSVC(random_state=0, tol=1e-5)
 ]
@@ -106,13 +111,32 @@ classifiers = [
 # Rodar com os mesmos algoritmos
 # Rodar com RFECV
 # endregion
+print('\n')
 for k_value in k_values:
-    print('K', k_value)
-    for method in methods:
-        print('Method', method.func_name)
-        X_new = SelectKBest(method, k=k_value).fit_transform(X, Y)
-        for classify in classifiers:
-            scores = cross_val_score(
-                classify, X_new, Y, cv=5, scoring='f1_macro')
-            print(type(classify).__name__, scores)
-    print('@@----@@')
+    # print('K')
+    # print(k_value)
+    # for method in methods:
+        # print('Method')
+        # print(method.func_name)
+    # X_new = SelectKBest(method, k=k_value).fit_transform(X, Y)
+    for classify in classifiers:
+        selector = RFECV(classify, step=1, cv=5,
+            min_features_to_select=k_value, scoring="f1_macro")
+        selector = selector.fit(X, Y)
+        scores = selector.grid_scores_
+        print(selector.support_)
+        print(selector.ranking_)
+        print(scores)
+        # scores = cross_val_score(
+        # classify, X_new, Y, cv=5, scoring='f1_macro')
+        # print(type(classify).__name__)
+        # print(scores)
+        mtd = {}
+        KResult = {}
+        clf = {}
+        clf[type(classify).__name__ + str(k_value)] = np.amax(scores)
+        # clf[type(classify).__name__ + str(k_value)] = np.mean(scores)
+        # mtd[method.func_name] = clf
+        mtd['RFECV'] = clf
+        # KResult[k_value] = mtd
+        print(mtd)
